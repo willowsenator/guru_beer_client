589b1e80e74b489684a9a4506d9530f4904c6996
willowsenator <willowsenator@gmail.com>
2022-07-24 19:37:37 +0200

    fix tests

--- src/test/java/com/willowsenator/guru_beer_client/web/model/client/BeerClientTest.java
+++ src/test/java/com/willowsenator/guru_beer_client/web/model/client/BeerClientTest.java
@@ -1,6 +1,7 @@
 package com.willowsenator.guru_beer_client.web.model.client;
 
 import com.willowsenator.guru_beer_client.web.model.BeerDto;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,6 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.fail;
 
 @SpringBootTest
 class BeerClientTest {
@@ -31,13 +33,23 @@ class BeerClientTest {
 
     @Test
     void updateBeer() {
-        var dto = BeerDto.builder().beerName("New Beer").build();
-        client.updateBeer(UUID.randomUUID(), dto);
+        try {
+            var dto = BeerDto.builder().beerName("New Beer").build();
+            client.updateBeer(UUID.randomUUID(), dto);
+        }
+        catch(Exception ex){
+            fail(ex.getMessage());
+        }
     }
 
     @Test
     void deleteBeer() {
-        client.deleteBeer(UUID.randomUUID());
+        try{
+            client.deleteBeer(UUID.randomUUID());
+        }
+        catch(Exception ex){
+            fail(ex.getMessage());
+        }
     }
 
 }
